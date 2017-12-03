34f8053ec70f24079b172b04e3b49e19f524ef35
Otavio Santana <otaviopolianasantana@gmail.com>
2017-12-03 10:21:13 -0300

    Updates the PersonController

--- src/main/java/br/com/danieldias/controller/PersonController.java
+++ src/main/java/br/com/danieldias/controller/PersonController.java
@@ -1,5 +1,10 @@
 package br.com.danieldias.controller;
 
+import br.com.danieldias.model.Person;
+import br.com.danieldias.persistence.PersonRepository;
+import org.jnosql.artemis.Database;
+import org.jnosql.artemis.DatabaseType;
+
 import javax.inject.Inject;
 import javax.mvc.Models;
 import javax.mvc.Viewable;
@@ -11,11 +16,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
-import org.bson.types.ObjectId;
-
-import br.com.danieldias.model.Person;
-import br.com.danieldias.persistence.MongoDAO;
-
 /**
  * @author daniel
  * github:Daniel-Dos
@@ -26,50 +26,51 @@ import br.com.danieldias.persistence.MongoDAO;
 @Path("mvc")
 public class PersonController {
 
-	@Inject
-	private Models models;
+    @Inject
+    private Models models;
 
-	@Inject
-	private MongoDAO<Person> peopleDao;
+    @Inject
+    @Database(DatabaseType.DOCUMENT)
+    private PersonRepository repository;
 
-	@GET
-	@Path("new")
-	public Viewable novo() {
-		return new Viewable("insert.jsp");
-	}
+    @GET
+    @Path("new")
+    public Viewable novo() {
+        return new Viewable("insert.jsp");
+    }
 
-	@GET
-	@Path("show")
-	@View("list.jsp")
-	public void list() {
-	  this.models.put("list", peopleDao.listAll());
-	}
+    @GET
+    @Path("show")
+    @View("list.jsp")
+    public void list() {
+        this.models.put("list", repository.findAll());
+    }
 
-	@POST
-	@Path("add")
-	public String adicionar(@BeanParam Person person) {
-		peopleDao.insert(person);
-		return "redirect:mvc/show";		
-	}
+    @POST
+    @Path("add")
+    public String adicionar(@BeanParam Person person) {
+        repository.save(person);
+        return "redirect:mvc/show";
+    }
 
-	@POST
-	@Path("update")
-	public String update(@BeanParam Person person) {
-		peopleDao.change(person);
-		return "redirect:mvc/show";
-	}
+    @POST
+    @Path("update")
+    public String update(@BeanParam Person person) {
+        repository.save(person);
+        return "redirect:mvc/show";
+    }
 
-	@GET
-	@Path("update/{_id}")
-	public Viewable update(@PathParam("_id") ObjectId _id) {
-		this.models.put("update",peopleDao.byId(_id));
-		return new Viewable("change.jsp",models);
-	}
+    @GET
+    @Path("update/{_id}")
+    public Viewable update(@PathParam("_id") String id) {
+        this.models.put("update", repository.findById(id));
+        return new Viewable("change.jsp", models);
+    }
 
-	@GET
-	@Path("remove/{_id}")
-	public String delete(@PathParam("_id") ObjectId _id) {
-	    peopleDao.delete(_id);
-		return "redirect:mvc/show";
-	}
+    @GET
+    @Path("remove/{_id}")
+    public String delete(@PathParam("_id") String id) {
+        repository.deleteById(id);
+        return "redirect:mvc/show";
+    }
 }
