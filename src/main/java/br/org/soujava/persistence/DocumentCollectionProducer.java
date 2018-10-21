8371264a1e2a9b9e242b291830f4ead88799af1e
Otavio Santana <otaviopolianasantana@gmail.com>
2018-10-21 07:02:17 -0700

    adds fixes

--- src/main/java/br/org/soujava/persistence/DocumentCollectionProducer.java
+++ src/main/java/br/org/soujava/persistence/DocumentCollectionProducer.java
@@ -1,13 +1,13 @@
 package br.org.soujava.persistence;
 
-import org.jnosql.diana.api.Settings;
+import org.jnosql.artemis.ConfigurationUnit;
 import org.jnosql.diana.api.document.DocumentCollectionManager;
 import org.jnosql.diana.api.document.DocumentCollectionManagerFactory;
-import org.jnosql.diana.api.document.DocumentConfiguration;
-import org.jnosql.diana.mongodb.document.MongoDBDocumentConfiguration;
+import org.jnosql.diana.mongodb.document.MongoDBDocumentCollectionManager;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 
 @ApplicationScoped
 public class DocumentCollectionProducer {
@@ -15,12 +15,13 @@ public class DocumentCollectionProducer {
 
     private static final String DOCUMENT_COLLECTION = "people";
 
+    @Inject
+	@ConfigurationUnit
+    private DocumentCollectionManagerFactory<MongoDBDocumentCollectionManager> managerFactory;
+
     @Produces
     @ApplicationScoped
     public DocumentCollectionManager getDocumentCollectionManager() {
-        DocumentConfiguration<?> configuration = new MongoDBDocumentConfiguration();
-        Settings settings = Settings.builder().put("mongodb-server-host-1", "localhost:27017").build();
-        DocumentCollectionManagerFactory managerFactory = configuration.get(settings);
         return managerFactory.get(DOCUMENT_COLLECTION);
     }
 }
